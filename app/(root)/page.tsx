d64eb53cfffc2c8d972cb36db60b1b67c3ca739f
JC MISA <johncarlomisa399@gmail.com>
2024-11-13 13:31:44 +0800

    make the search functional by getting startups based on typed title, author, or category

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,6 @@
 import SearchForm from "@/components/custom/SearchForm";
 import StartupCard, { StartupTypeCard } from "@/components/custom/StartupCard";
-import { client } from "@/sanity/lib/client";
+import { sanityFetch, SanityLive } from "@/sanity/lib/live";
 import { STARTUPS_QUERY } from "@/sanity/lib/queries";
 
 export default async function Home({
@@ -10,41 +10,8 @@ export default async function Home({
 }) {
   const query = (await searchParams).query;
 
-  const posts = await client.fetch(STARTUPS_QUERY);
-  //   {
-  //     _createdAt: new Date(),
-  //     views: 55,
-  //     author: { _id: 1, name: "John Carlo" },
-  //     _id: 1,
-  //     description: "This is a description",
-  //     image:
-  //       "https://cdn.prod.website-files.com/66e3ffddf1c6e55cc8c157f6/670e5eb8e61989bc964945e5_655b5e2e1972c2cc19a3c190_startup-business-model.png",
-  //     category: "Robots",
-  //     title: "We Robots",
-  //   },
-  //   {
-  //     _createdAt: new Date(),
-  //     views: 55,
-  //     author: { _id: 2, name: "John Moniel" },
-  //     _id: 2,
-  //     description: "This is a description",
-  //     image:
-  //       "https://cdn.prod.website-files.com/66e3ffddf1c6e55cc8c157f6/670e5eb8e61989bc964945e5_655b5e2e1972c2cc19a3c190_startup-business-model.png",
-  //     category: "Robots",
-  //     title: "We Robots",
-  //   },
-  //   {
-  //     _createdAt: new Date(),
-  //     views: 55,
-  //     author: { _id: 3, name: "John Vincent" },
-  //     _id: 3,
-  //     description: "This is a description",
-  //     image:
-  //       "https://cdn.prod.website-files.com/66e3ffddf1c6e55cc8c157f6/670e5eb8e61989bc964945e5_655b5e2e1972c2cc19a3c190_startup-business-model.png",
-  //     category: "Robots",
-  //     title: "We Robots",
-  //   },
-  // ];
+  const params = { search: query || null };
+  const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY, params }); // to make a real-time fetching of data
 
   return (
     <>
@@ -76,6 +43,8 @@ export default async function Home({
           )}
         </ul>
       </section>
+
+      <SanityLive />
     </>
   );
 }
--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -1,10 +1,12 @@
-import { createClient } from 'next-sanity'
+import { createClient } from "next-sanity";
 
-import { apiVersion, dataset, projectId } from '../env'
+import { apiVersion, dataset, projectId } from "../env";
 
 export const client = createClient({
   projectId,
   dataset,
   apiVersion,
-  useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
-})
+  // when set to true, it will wait for 60 seconds after a data changed from the database before rendering it. Meaning it will fetch data from cache first and right after 60 seconds, it will revalidate the data and fetch new data from server
+  // when set to false, it will retrieve data in real-time imediately from the server
+  useCdn: false,
+});
